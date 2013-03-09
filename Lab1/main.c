eecca8f7edb4852a0f00f8435854d71b7b0b72a7
Kyle Rutledge <mojofire@gmail.com>
2013-03-09 14:26:45 -0600

    Blank project for Lab 1

---
+++ Lab1/main.c
@@ -0,0 +1,20 @@
+/* Lab1 - an application for the Pololu Orangutan SVP
+ *
+ * This application uses the Pololu AVR C/C++ Library.  For help, see:
+ * -User's guide: http://www.pololu.com/docs/0J20
+ * -Command reference: http://www.pololu.com/docs/0J18
+ *
+ * Created: 3/9/2013 2:23:10 PM
+ *  Author: Kyle
+ */
+
+#include <pololu/orangutan.h>
+
+int main()
+{
+
+	while(1)
+	{
+        
+	}
+}
