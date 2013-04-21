b2b5141c78decb188dc845e67ea4fe257e58a4e3
Kyle Rutledge <mojofire@gmail.com>
2013-04-21 13:29:35 -0500

    Many changes and refactoring, now it is at least stable (still some more refactoring needed)

--- Lab2/real_time_data_plot.m
+++ Lab2/real_time_data_plot.m
@@ -1,14 +1,9 @@
 %% Real time data collection example
-% This MATLAB script generates a real time plot of voltage values collected
-% from an Agilent(TM) 34401A Digital Multimeter over the serial
-% port(RS232). The data is collected and plotted until the predefined stop
-% time is reached. This example also demonstrates automating a query based
-% interaction with an instrument while monitoring the results live.
-%
-% This script can be modified to be used on any platform by changing the
-% serialPort variable.
+% This MATLAB script generates a real time plot of a PD controller over the
+% serial port. It also has control to set PD constants and the relative
+% target position.
 % 
-% Example:-
+% Example COM ports:-
 %
 % On Linux:     serialPort = '/dev/ttyS0';
 %
@@ -17,220 +12,286 @@
 % On Windows:   serialPort = 'COM1';
 %
 %
-% The script may also be updated to use any instrument/device to collect
-% real time data. You may need to update the SCPI commands based on
-% information in the instrument's programming manual.
-%
 % To generate a report of this entire script, you may use the PUBLISH
 % command at the MATLAB(R) command line as follows: 
 % 
-% publish(real_time_data_plot);
+% publish('function_name.m');
 % 
-% Author: Ankit Desai
-% Copyright 2009 - The MathWorks, Inc.
+% Author: Kyle Rutlege
+% Copyright 2013
 
 function real_time_data_plot
+    %% Serial port constants
+    COM_PORT = 'COM7';
+    BAUD_RATE = 256000;
+    MEM_PREALLOCATE = 100000;
+    
+    %% Preallocate variables to graph
+    curX  = nan(1, MEM_PREALLOCATE);
+    curPe = nan(1, MEM_PREALLOCATE);
+    curKp = nan(1, MEM_PREALLOCATE);
+    curPr = nan(1, MEM_PREALLOCATE);
+    curPm = nan(1, MEM_PREALLOCATE);
+    curT  = nan(1, MEM_PREALLOCATE);
+    curVm = nan(1, MEM_PREALLOCATE);
+    curKd = nan(1, MEM_PREALLOCATE);
 
     %% Set up the figure window
-    x = nan(1, 10000);
-    Pe = nan(1, 10000);
-    Kp = nan(1, 10000);
-    Pr = nan(1, 10000);
-    Pm = nan(1, 10000);
-    T = nan(1, 10000);
-    Vm = nan(1, 10000);
-    Kd = nan(1, 10000);
-    
     figureHandle = figure('NumberTitle','off',...
-        'Position', [200, 100, 1500, 900], ...
+        'Position', [150, 75, 1500, 900], ...
         'Name','PD outputs',...
-        'Visible','off');
+        'Visible','on');
 
     % Set axes
-    axesPe = subplot(3,2,1, 'Parent', figureHandle, 'Visible', 'off');
-    axesKp = subplot(3,2,2, 'Parent', figureHandle, 'Visible', 'off');
-    axesP  = subplot(3,2,3, 'Parent', figureHandle, 'Visible', 'off');
-    axesT  = subplot(3,2,4, 'Parent', figureHandle, 'Visible', 'off');
-    axesV  = subplot(3,2,5, 'Parent', figureHandle, 'Visible', 'off');
-    axesKd = subplot(3,2,6, 'Parent', figureHandle, 'Visible', 'off');
+    axesPe = subplot(3,2,1, 'Parent', figureHandle);
+    plotPe = plot(axesPe, curX, curPe);
 
-    hold on;
+    axesKp = subplot(3,2,2, 'Parent', figureHandle);
+    plotKp = plot(axesKp, curX, curKp);
 
-    plotPr = plot(axesP, 0,0, 'color', 'blue');
-    plotPm = plot(axesP, 0,0, 'color', 'red');
+    axesP  = subplot(3,2,3, 'Parent', figureHandle);
+    plotPr = plot(axesP, curX, curPr);
+    hold on
+    plotPm = plot(axesP, curX, curPm);
+
+    axesT  = subplot(3,2,4, 'Parent', figureHandle);
+    plotT  = plot(axesT, curX, curT);
+
+    axesV  = subplot(3,2,5, 'Parent', figureHandle);
+    plotVm = plot(axesV, curX, curVm);
+
+    axesKd = subplot(3,2,6, 'Parent', figureHandle);
+    plotKd = plot(axesKd, curX, curKd);
+    
+    %% Subplot strings
+    
+    ylabel(axesPe, 'Integer');
+    title(axesPe, 'Position Error (Pe)');
+    
+    ylabel(axesKp, 'Integer');
+    title(axesKp, 'K - Proportional (Kp)');
+    
+    ylabel(axesP, 'Integer');
+    title(axesP, 'Position (Pr,Pm)');
+    
+    ylabel(axesT, 'Integer');
+    title(axesT, 'Torque (T)');
+    
+    ylabel(axesV, 'Integer');
+    title(axesV, 'Velocity (Vm)');
+    
+    ylabel(axesKd, 'Integer');
+    title(axesKd, 'K - Derivative (Kd)');
+
+    %% Other plot data
+    
+%     plotPr = plot(axesP, x,Pr);
+%     plotPm = plot(axesP, x,Pm);
 %     legend( [plotPr,plotPm], 'Position Ref', 'Position Target',  'Location', 'SouthWest');
 
-    hold off;
+%     plotPe = plot(axesPe,x,Pe);
+%     plotKp = plot(axesKp,x,Kp);
+%     plotT  = plot(axesT, x,T);
+%     plotVm = plot(axesV, x,Vm);
+%     plotKd = plot(axesKd,x,Kd);
 
-    plotPe = plot(axesPe,0,0, 'color', 'blue');
-    plotKp = plot(axesKp,0,0, 'color', 'blue');
-    plotT  = plot(axesT, 0,0, 'color', 'blue');
-    plotVm = plot(axesV, 0,0, 'color', 'blue');
-    plotKd = plot(axesKd,0,0, 'color', 'blue');
-
-    legend( plotPe, 'Position Error',   'Location', 'SouthWest');
-    legend( plotKp, 'K - Prop',         'Location', 'SouthWest');
-    legend( plotT,  'Torque',           'Location', 'SouthWest');
-    legend( plotVm, 'Velocity',         'Location', 'SouthWest');
-    legend( plotKd, 'K - Deriv',        'Location', 'SouthWest');
-
-    % Create xlabel
-    xlabel('Time');
-
-    % Create ylabel
-    ylabel('Integer');
-
-    % Create title
-    title('Data From Organutan');
-
-    %% Create the serial object
-    com = serial('COM7');
-    set(com, 'BaudRate', 256000)
+%     legend( plotPe, 'Position Error',   'Location', 'SouthWest');
+%     legend( plotKp, 'K - Prop',         'Location', 'SouthWest');
+%     legend( plotT,  'Torque',           'Location', 'SouthWest');
+%     legend( plotVm, 'Velocity',         'Location', 'SouthWest');
+%     legend( plotKd, 'K - Deriv',        'Location', 'SouthWest');
     
-    %% Button to stop the program
+    %% GUI display constants
+    buttonWidth = 100;
+    buttonHeight = 50;
+    textWidth = 60;
+    textHeight = 25;
+    valueWidth = 75;
+    valueHeight = 25;
     
-    % Buttons
+    %% Buttons
+    cumButtonHeight = 0;
     closeButton = uicontrol( figureHandle, ...
         'Style', 'pushbutton', ...
         'String', 'Close', ...
-        'Callback', {@stopReading,com}, ...
-        'Position', [75 0 75 25]);
+        'Callback', @stopReading, ...
+        'Position', [valueWidth cumButtonHeight buttonWidth buttonHeight]);
+    cumButtonHeight = cumButtonHeight + buttonHeight;
     clearButton = uicontrol( figureHandle, ...
         'Style', 'pushbutton', ...
         'String', 'Clear', ...
-        'Callback', {@clearGraph,plotPe,plotKp,plotPr,plotPm,plotT,plotVm,plotKd}, ...
-        'Position', [75 25 75 25]);
+        'Callback', @clearGraph, ...
+        'Position', [valueWidth cumButtonHeight buttonWidth buttonHeight]);
+    cumButtonHeight = cumButtonHeight + buttonHeight;
     
-    % Texts
-    uicontrol( figureHandle, ...
-        'Style', 'text', ...
-        'String', 'Pe', ...
-        'Position', [75 50 75 25]);
-    uicontrol( figureHandle, ...
-        'Style', 'text', ...
-        'String', 'Kp', ...
-        'Position', [75 75 75 25]);
-    uicontrol( figureHandle, ...
-        'Style', 'text', ...
-        'String', 'Pr', ...
-        'Position', [75 100 75 25]);
-    uicontrol( figureHandle, ...
-        'Style', 'text', ...
-        'String', 'Pm', ...
-        'Position', [75 125 75 25]);
-    uicontrol( figureHandle, ...
-        'Style', 'text', ...
-        'String', 'T', ...
-        'Position', [75 150 75 25]);
-    uicontrol( figureHandle, ...
-        'Style', 'text', ...
-        'String', 'Vm', ...
-        'Position', [75 175 75 25]);
+    function stopReading(hObj,event)
+        enableLoop = 0;
+    end
+    
+    function clearGraph(hObj,event)
+        clearReq = 1;
+    end
+
+    %% Texts
+    cumTextHeight = cumButtonHeight;
     uicontrol( figureHandle, ...
         'Style', 'text', ...
         'String', 'Kd', ...
-        'Position', [75 200 75 25]);
+        'Position', [valueWidth cumTextHeight textWidth textHeight]);
+    cumTextHeight = cumTextHeight + textHeight;
+    uicontrol( figureHandle, ...
+        'Style', 'text', ...
+        'String', 'Vm', ...
+        'Position', [valueWidth cumTextHeight textWidth textHeight]);
+    cumTextHeight = cumTextHeight + textHeight;
+    uicontrol( figureHandle, ...
+        'Style', 'text', ...
+        'String', 'Pr', ...
+        'Position', [valueWidth cumTextHeight textWidth textHeight]);
+    cumTextHeight = cumTextHeight + textHeight;
+    uicontrol( figureHandle, ...
+        'Style', 'text', ...
+        'String', 'T', ...
+        'Position', [valueWidth cumTextHeight textWidth textHeight]);
+    cumTextHeight = cumTextHeight + textHeight;
+    uicontrol( figureHandle, ...
+        'Style', 'text', ...
+        'String', 'Pm', ...
+        'Position', [valueWidth cumTextHeight textWidth textHeight]);
+    cumTextHeight = cumTextHeight + textHeight;
+    uicontrol( figureHandle, ...
+        'Style', 'text', ...
+        'String', 'Kp', ...
+        'Position', [valueWidth cumTextHeight textWidth textHeight]);
+    cumTextHeight = cumTextHeight + textHeight;
+    uicontrol( figureHandle, ...
+        'Style', 'text', ...
+        'String', 'Pe', ...
+        'Position', [valueWidth cumTextHeight textWidth textHeight]);
+    cumTextHeight = cumTextHeight + textHeight;
 
-    % Values
-    PeValue = uicontrol( figureHandle, ...
-        'Style', 'edit', ...
-        'Enable', 'off', ...
-        'Position', [0 50 75 25]);
-    KpValue = uicontrol( figureHandle, ...
-        'Style', 'edit', ...
-        'Enable', 'off', ...
-        'Position', [0 75 75 25]);
-    PrValue = uicontrol( figureHandle, ...
-        'Style', 'edit', ...
-        'Enable', 'off', ...
-        'Position', [0 100 75 25]);
-    PmValue = uicontrol( figureHandle, ...
-        'Style', 'edit', ...
-        'Enable', 'off', ...
-        'Position', [0 125 75 25]);
-    TValue = uicontrol( figureHandle, ...
-        'Style', 'edit', ...
-        'Enable', 'off', ...
-        'Position', [0 150 75 25]);
-    VmValue = uicontrol( figureHandle, ...
-        'Style', 'edit', ...
-        'Enable', 'off', ...
-        'Position', [0 175 75 25]);
+    %% Values
+    cumValueHeight = cumButtonHeight;
     KdValue = uicontrol( figureHandle, ...
         'Style', 'edit', ...
         'Enable', 'off', ...
-        'Position', [0 200 75 25]);
+        'Position', [0 cumValueHeight valueWidth valueHeight]);
+    cumValueHeight = cumValueHeight + valueHeight;
+    VmValue = uicontrol( figureHandle, ...
+        'Style', 'edit', ...
+        'Enable', 'off', ...
+        'Position', [0 cumValueHeight valueWidth valueHeight]);
+    cumValueHeight = cumValueHeight + valueHeight;
+    PrValue = uicontrol( figureHandle, ...
+        'Style', 'edit', ...
+        'Enable', 'off', ...
+        'Position', [0 cumValueHeight valueWidth valueHeight]);
+    cumValueHeight = cumValueHeight + valueHeight;
+    TValue = uicontrol( figureHandle, ...
+        'Style', 'edit', ...
+        'Enable', 'off', ...
+        'Position', [0 cumValueHeight valueWidth valueHeight]);
+    cumValueHeight = cumValueHeight + valueHeight;
+    PmValue = uicontrol( figureHandle, ...
+        'Style', 'edit', ...
+        'Enable', 'off', ...
+        'Position', [0 cumValueHeight valueWidth valueHeight]);
+    cumValueHeight = cumValueHeight + valueHeight;
+    KpValue = uicontrol( figureHandle, ...
+        'Style', 'edit', ...
+        'Enable', 'off', ...
+        'Position', [0 cumValueHeight valueWidth valueHeight]);
+    cumValueHeight = cumValueHeight + valueHeight;
+    PeValue = uicontrol( figureHandle, ...
+        'Style', 'edit', ...
+        'Enable', 'off', ...
+        'Position', [0 cumValueHeight valueWidth valueHeight]);
+    cumValueHeight = cumValueHeight + valueHeight;
 
-    %%
-    
+    %% Setup the serial port
+    com = serial(COM_PORT);
+    set(com, 'BaudRate', BAUD_RATE)
     fclose(instrfind);
     fopen(com);
     
-    %% Collect data
+    %% Initialization before collecting data
     enableLoop = 1;
+    clearReq = 1;
     tick = 1;
-
-    while ( enableLoop == 1 )
-            com_input = fscanf(com,'%d,%d,%d');
-
-            x1 = get(plotPe, 'XData');
-            Kp1 = get(plotKp, 'YData');
-%             Pr1 = get(plotPr, 'YData');
-%             Pm1 = get(plotPm, 'YData');
-            T1 = get(plotT, 'YData');
-            Vm1 = get(plotVm, 'YData');
-            Kd1 = get(plotKd, 'YData');
-
-            if isempty(x1) || isempty(Kp1) || isempty(T1) || isempty(Vm1) || isempty(Kd1) % isempty(Pr1) || isempty(Pm1)
-                tick = 1;
-
-                x = zeros(1, 10000);
-                Pe = zeros(1, 10000);
-                Kp = zeros(1, 10000);
-                Pr = zeros(1, 10000);
-                Pm = zeros(1, 10000);
-                T = zeros(1, 10000);
-                Vm = zeros(1, 10000);
-                Kd = zeros(1, 10000);
-            end
     
-            x(tick) = tick;
+    %% Collect data 
+    while ( enableLoop == 1 )
+            %% Conditionally clear the graph array contents
+            if ( clearReq == 1)
+                clearReq = 0;
 
-            Kp(tick) = com_input(1);
-            Pr(tick) = com_input(3);
-            Pm(tick) = com_input(1);
-            T(tick) = com_input(2);
-            Vm(tick) = com_input(3);
-            Kd(tick) = com_input(3);
-
-            Pe(tick) = Pm(tick) - Pr(tick);
-            
-            if ( mod(tick,4) == 0 )
-                set(plotPe,'YData',Pe,'XData',x);
-                set(plotKp,'YData',Kp,'XData',x);
-    %             set(plotPr,'YData',Pr,'XData',x);
-    %             set(plotPm,'YData',Pm,'XData',x);
-                set(plotT,'YData',T,'XData',x);
-                set(plotVm,'YData',Vm,'XData',x);
-                set(plotKd,'YData',Kd,'XData',x);
-                set(figureHandle,'Visible','on');
-
-                set(PeValue, 'String', com_input(1)-com_input(2));
-                set(KpValue, 'String', com_input(1));
-    %             set(PrValue, 'String', com_input(3));
-    %             set(PmValue, 'String', com_input(1));
-                set(TValue, 'String', com_input(2));
-                set(VmValue, 'String', com_input(3));
-                set(KdValue, 'String', com_input(3));
+                tick  = 1;
+                curX  = nan(1, MEM_PREALLOCATE);
+                curPe = nan(1, MEM_PREALLOCATE);
+                curKp = nan(1, MEM_PREALLOCATE);
+                curPr = nan(1, MEM_PREALLOCATE);
+                curPm = nan(1, MEM_PREALLOCATE);
+                curT  = nan(1, MEM_PREALLOCATE);
+                curVm = nan(1, MEM_PREALLOCATE);
+                curKd = nan(1, MEM_PREALLOCATE);
             end
+            %% Read the device and parse the data
+            com_input = fscanf(com,'%d,%d,%d,%d,%d,%d');
 
-            clear x1;
-            clear Kp1;
-            clear Pr1;
-            clear Pm1;
-            clear TV1;
-            clear Vm1;
-            clear Kd1;
+            curX(tick) = tick;
+
+            lastKp = com_input(1);
+            lastPr = com_input(1);
+            lastPm = com_input(2);
+            lastT  = com_input(1);
+            lastVm = com_input(1);
+            lastKd = com_input(1);
+
+            lastPe = lastPm - lastPr;
+            
+            %% Add to the graph arrays
+            curPe(tick) = lastPe;
+            curKp(tick) = lastKp;
+            curPr(tick) = lastPr;
+            curPm(tick) = lastPm;
+            curT(tick)  = lastT;
+            curVm(tick) = lastVm;
+            curKd(tick) = lastKd;
+            
+            %% Conditionally update the GUI
+            % This should probably be done in a timer callback instead of here
+            if ( mod(tick,8) == 0 )
+                set(plotPe,'YData',curPe,'XData',curX);
+                set(plotKp,'YData',curKp,'XData',curX);
+%                 set(plotPm,'YData',curPm,'XData',curX);
+%                 set(plotPr,'YData',curPr,'XData',curX);
+                set(plotT, 'YData',curT, 'XData',curX);
+                set(plotVm,'YData',curVm,'XData',curX);
+                set(plotKd,'YData',curKd,'XData',curX);
+
+%                 plotPe = plot(axesPe, curX, curPe);
+%                 plotKp = plot(axesKp, curX, curKp);
+                set(axesP,'NextPlot','replacechildren');
+%                 plotPr = plot(axesP,  curX, curPr);
+%                 hold on
+%                 plotPm = plot(axesP,  curX, curPm);
+%                 plotPrPm = plot(axesP,  [curX curX], [curPr curPm]);
+                plotPrPm = plot(axesP,  curX, curPr, 'r', curX, curPm, 'b');
+%                 plotT  = plot(axesT,  curX, curT);
+%                 plotVm = plot(axesV,  curX, curVm);
+%                 plotKd = plot(axesKd, curX, curKd);
+
+                set(PeValue, 'String', lastPe);
+                set(KpValue, 'String', lastKp);
+                set(PrValue, 'String', lastPr);
+                set(PmValue, 'String', lastPm);
+                set(TValue,  'String', lastT);
+                set(VmValue, 'String', lastVm);
+                set(KdValue, 'String', lastKd);
+
+                set(figureHandle,'Visible','on');
+%                 drawnow;
+            end
             
             tick = tick + 1;
     end
@@ -242,20 +303,5 @@ function real_time_data_plot
     
     %% Clean up the figure
     close(figureHandle);
-end
-
-function stopReading(hObj,event,serial)
-    %% Stop reading
-    fclose(serial);
-end
-
-function clearGraph(hObj,event,p1,p2,p3,p4,p5,p6,p7)
-    %% Clear contents of data used to graph
-    set(p1,'XData',[],'YData',[]);
-    set(p2,'XData',[],'YData',[]);
-%     set(p3,'XData',[],'YData',[]);
-%     set(p4,'XData',[],'YData',[]);
-    set(p5,'XData',[],'YData',[]);
-    set(p6,'XData',[],'YData',[]);
-    set(p7,'XData',[],'YData',[]);
+    
 end
