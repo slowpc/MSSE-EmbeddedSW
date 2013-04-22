7909c81e668970fd776e06de73f64beae23d335d
Kyle Rutledge <mojofire@gmail.com>
2013-04-21 23:19:01 -0500

    Changed all computations to float, all values are now in terms of degrees, changed main loop back to delay in ms instead of us.

--- Lab2/main.c
+++ Lab2/main.c
@@ -17,35 +17,27 @@
 //#include "menu.h"
 
 #define BUFFER_SIZE 64
-#define LOOP_DELAY_US 10000
+#define LOOP_DELAY_MS 50
 #define MAX_INT_OUTPUT 100
 #define USB_BAUD_RATE 256000
 
 // Position
-#define POSITION_ERROR_MIN 3
-#define POSITION_ERROR_MAX 30
+#define DEG_PER_COUNT (360.0f / 64.0f)
+#define POSITION_ERROR_MIN 10
+#define POSITION_ERROR_MAX 45
 
 // Velocity
 #define V_ITER_THRESH 15
 
 // Motor
-#define MOTOR_SPEED_MIN                 16
-#define MOTOR_SPEED_MAX                 50
+#define MOTOR_SPEED_MIN                 20
+#define MOTOR_SPEED_MAX                 100
 
 // Encoder pin mapping
 #define PIN_ENCODER_1A                  IO_A2
 #define PIN_ENCODER_1B                  IO_A3
 #define PIN_ENCODER_2A                  IO_A0
 #define PIN_ENCODER_2B                  IO_A1
-
-// Encoder
-#define ENCODER_COUNT_HYS_RANGE         8
-#define ENCODER_COUNT_PER_REV           32
-#define ENCODER_NUM_REVS                2
-#define ENCODER_ABS_MAX                 ( ENCODER_COUNT_PER_REV * ENCODER_NUM_REVS )
-#define ENCODER_MIN                     -ENCODER_ABS_MAX
-#define ENCODER_MAX                     ENCODER_ABS_MAX
-#define ENCODER_START                   -ENCODER_ABS_MAX
 
 
 void print_usb_char( char buffer );
@@ -83,16 +75,15 @@ void wait_for_sending_to_finish( void )
 
 int main()
 {
-    signed int Pe, Pr, Pm, Vm, T, Kp, Kd;
-    unsigned int Pr_status, Pm_status, Vm_status, T_status, Kp_status, Kd_status;
+    signed int Pm_count;
+    float Pe, Pr, Pm, Vm, T, Kp, Kd;
     char buffer[BUFFER_SIZE];
-    
-    signed int Kp_int;
-    signed int Kd_int;
 
     unsigned int v_iter = 0;
     unsigned int v_iter_last_pos = 0;
     unsigned int v_initialized = 0;
+    
+    unsigned int speed, direction;
 
     Pe = Pr = Pm = Vm = T=  Kp = Kd = 0;
 
@@ -106,13 +97,17 @@ int main()
     // Initialize the encoders and specify the four input pins, first two are for motor 1, second two are for motor 2
     encoders_init( PIN_ENCODER_1A, PIN_ENCODER_1B, PIN_ENCODER_2A, PIN_ENCODER_2B );
 
-    Pr = 64;
+    // Dummy values for the time being
+    Pr = 360;
+    Kp = 0.05f;
+    Kd = -.05f;
 
 	while(1)
 	{
         // Calc current position
-        Pm        = encoders_get_counts_m2();
-        Pm_status = encoders_check_error_m2();
+        Pm_count  = encoders_get_counts_m2();
+
+        Pm = Pm_count * DEG_PER_COUNT;
 
         // Calc velocity
         if ( v_iter++ > V_ITER_THRESH )
@@ -128,34 +123,15 @@ int main()
         if ( Pe > POSITION_ERROR_MAX )
             Pe = POSITION_ERROR_MAX;
 
+        if ( Pe < -POSITION_ERROR_MAX )
+            Pe = -POSITION_ERROR_MAX;
+
 
         // Torque
-        T = (int) (Kp * 1.0f * Pe - Kd * 1.0f * Vm);
+        T = Kp * Pe - Kd * Vm;
 
-/*
-        if ( Pm < MOTOR_SPEED_MIN )
-        {
-            direction = 1;
-            speed = MOTOR_SPEED_MIN;
-        }
-        else
-        {
-            speed = Pm;
-            
-            if ( direction == 1 && Pm > 100)
-            {
-                direction = 0;
-                Pr = 32;
-            }
-            else if ( direction == 0 && Pm < 32 )
-            {
-                direction = 1;
-                Pr = 100;
-            }
-        }*/
+        
 
-        if (Pe > POSITION_ERROR_MIN && T < MOTOR_SPEED_MIN )
-            T = MOTOR_SPEED_MIN;
 
         if ( T < -MOTOR_SPEED_MAX )
             T = -MOTOR_SPEED_MAX;
@@ -163,15 +139,33 @@ int main()
         if ( T > MOTOR_SPEED_MAX )
             T = MOTOR_SPEED_MAX;
 
-        set_motors( 0, T );
+        if ( T < 0 )
+        {
+            speed = -T;
+            direction = 0;
+        }
+        else
+        {
+            speed = T;
+            direction = 1;
+        }
 
-        // Dummy values for the time being
-        Kp = 10;
-        Kd = -15;
-        snprintf( buffer, BUFFER_SIZE, "%d,%d,%d,%d,%d,%d,%d\r\n", Pe, Pr, Pm, Vm, T, Kp, Kd );
+        // Force the motor to the minimum amount if it hasn't reached the goal yet
+        if (abs(Pe) > POSITION_ERROR_MIN && speed < MOTOR_SPEED_MIN )
+            speed = MOTOR_SPEED_MIN;
+
+        // TODO: Replace with setting registers
+        signed int T_temp;
+        T_temp = ( direction == 1 ) ? speed : -speed;
+        set_motors( 0, T_temp );
+
+        snprintf( buffer, BUFFER_SIZE, "v,%d,%d,%d,%d,%d,%d,%d\r\n", (signed int)Pe, (signed int)Pr, (signed int)Pm, (signed int)Vm, (signed int)T_temp, (signed int)Kp, (signed int)Kd );
 
         print_usb( buffer );
 
-        delay_us( LOOP_DELAY_US );
+//        serial_check();
+//        check_for_new_bytes_received();
+
+        delay_ms( LOOP_DELAY_MS );
 	}
 }
